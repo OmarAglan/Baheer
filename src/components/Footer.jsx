58522953b57151a5f38c70c3126ae0ca1ae8589a
Omar Aglan <omar.aglan91@yahoo.com>
2025-03-10 06:03:07 +0200

    Simple Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
+    <div className='p-10 mt-10 border-t text-center dark:border-t-gray-700 border-gray-200'>
         <footer>
-            <p>Footer</p>
+            <p className='text-gray-500 dark:text-gray-400'>© 2025 Baheer. All rights reserved.</p>
         </footer>
     </div>
   )
