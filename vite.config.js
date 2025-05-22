374df839f361e3c792fc0f542198979ec809a8f2
Omar Aglan <omar.aglan91@yahoo.com>
2025-05-22 09:46:19 +0300

    Chore: Update deploy.yml for clarity and consistency; reorder imports in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/baheer/', // Replace 'baheer' with your repository name if different
+  base: '/Baheer/', // Replace 'baheer' with your repository name if different
   plugins: [react(), tailwindcss()]
-})
+});
